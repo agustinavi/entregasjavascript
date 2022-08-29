77c14f3dbcd6d65034ce12b198e7777e6860a672
agustinavi <atuuart@gmail.com>
2022-08-29 19:36:48 -0300

    first commit

---
+++ Viñes js entrega 1/bucle.js
@@ -0,0 +1,7 @@
+for(let i = 0; i <= 100; i++){
+    let nombre = prompt("Ingrese su nombre para sacar turno");
+    let mensaje = `${nombre} , su lugar en la fila es el #${i}`;
+    alert(mensaje)
+}
+
+alert("Se acabaron los turnos")
---
+++ Viñes js entrega 1/condicional.js
@@ -0,0 +1,10 @@
+
+let usuario = prompt("Ingrese el usuario");
+
+while(usuario.toLowerCase != "Agustina"){
+    alert("Usuario incorrecto")
+    usuario = prompt("Ingrese el usuario")
+}
+
+alert("Bienvenida Agustina!")
+
